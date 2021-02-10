9ee1339cbfcd3ce6be5fa50185d1a62f474ffe1c
munireddy <munisekhar.reddy@gmail.com>
2021-02-09 20:48:29 -0500

    Added two line for adding number

--- file1.c
+++ file1.c
@@ -4,5 +4,7 @@ int main () {
       printf("This is a second sample\n");
       printf("This is a Third sample\n");
       int num1 = 10;
-      printf("The value of i is: %d\n", num1); 
+      printf("The value of num1 is: %d\n", num1);
+      num1 = num1 + 100;
+      printf("100 is added to num1 : %d\n", num1);
 }

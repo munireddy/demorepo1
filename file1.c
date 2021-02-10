6c8055a20aaa353953192e6a1f50ccba7e38002e
munireddy <munisekhar.reddy@gmail.com>
2021-02-09 20:46:02 -0500

    Update file1.c

--- file1.c
+++ file1.c
@@ -3,4 +3,6 @@ int main () {
       printf("This is a first sample\n");
       printf("This is a second sample\n");
       printf("This is a Third sample\n");
+      int num1 = 10;
+      printf("The value of i is: %d\n", num1); 
 }
